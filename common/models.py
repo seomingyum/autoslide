86f9f13961725707187020ec948d4ac465934b48
seomingyum <smg0712@naver.com>
2023-03-28 18:04:35 +0900

    4_commit

--- common/models.py
+++ common/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=20, default="")
-    phonenumber = models.CharField(max_length=20, null=True)
+    nickname = models.CharField(max_length=50)
+    phoneNumberRegex = RegexValidator(regex=r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    phone = models.CharField(validators=[phoneNumberRegex], max_length=11, unique=True, default=True)
     first_name = None
     last_name = None
